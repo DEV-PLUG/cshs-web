419a7aa2531100f8615757bbd8107e1bda869efe
JiHwan Choi <jhcplace@daum.net>
2025-05-20 08:38:48 +0900

    [Update] 활동 나가기 기능 삭제

--- app/d/activity/activity-detail.tsx
+++ app/d/activity/activity-detail.tsx
@@ -43,31 +43,6 @@ export default function ActivityDetail({ data, fn }:{ data:any, fn():void }) {
       }
     });
   }
-  async function quitActivity() {
-    if(loading) return;
-    setLoading(true);
-
-    await fetch(`/api/activity/quit`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        id: data.id
-      })
-    })
-    .then((response) => response.json())
-    .then((response) => {
-      setLoading(false);
-      if(response.success === true) {
-        fn();
-        dispatch(setNotification({ type: 'success', text: '해당 활승에서 제외됐어요' }));
-        data.mutateActivity();
-      } else {
-        dispatch(setNotification({ type: 'error', text: errorMessage.unknown }));
-      }
-    });
-  }
   
   const [fullMemberModal, setFullMemberModal] = useState(false);
 
@@ -117,13 +92,6 @@ export default function ActivityDetail({ data, fn }:{ data:any, fn():void }) {
                   <div onClick={() => deleteActivity()} className="w-7 h-7 bg-blue-500/20 hover:bg-blue-600/20 transition-colors cursor-pointer flex items-center justify-center rounded-lg">
                     <svg className="w-5 h-5 stroke-blue-500" fill="none" strokeWidth={2} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                    </svg>
-                  </div>
-                </OpacityAnimation> }
-                { user?.success === true && data.relation.some((relation:{ user: { id: number } }) => relation.user.id === user.user.id) && <OpacityAnimation>
-                  <div onClick={() => quitActivity()} className="w-7 h-7 bg-blue-500/20 hover:bg-blue-600/20 transition-colors cursor-pointer flex items-center justify-center rounded-lg">
-                    <svg className="w-5 h-5 stroke-blue-500" fill="none" strokeWidth={2} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
                     </svg>
                   </div>
                 </OpacityAnimation> }
